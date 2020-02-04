79a022e865b93f6b78b29fc822f1188ababee7da
Admin <Admin@DESKTOP-3DISMH6>
2020-02-04 19:20:52 +0000

    ApplicationContext Added

--- src/main/java/com/savant/SpringBootProject/SpringBootProjectApplication.java
+++ src/main/java/com/savant/SpringBootProject/SpringBootProjectApplication.java
@@ -1,13 +1,21 @@
 package com.savant.SpringBootProject;
 
+import java.util.Arrays;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringBootProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootProjectApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(SpringBootProjectApplication.class, args);
+		String[] beanNames = ctx.getBeanDefinitionNames();
+		Arrays.sort(beanNames);
+		for(String beanName : beanNames){
+			System.out.println(beanName);
+		}
 	}
 
 }
